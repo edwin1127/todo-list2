315cd4c63480ccd143f8abb41e84334456923b98
Edwin Corzo <edwin11ramirez@gmail.com>
2015-04-22 16:37:14 -0700

    milestone 5

--- includes/connect.php
+++ includes/connect.php
@@ -1,10 +1,10 @@
 <?php
 //why is this important? why does localhost have to be first?
-$mysqli = new mysqli('localhost', 'root', 'tasks');
+$mysqli = new mysqli('localhost', 'root', 'todo');
 
 if ($mysqli->connect_error) {
-	die('Connect Error ('. $mysqli_->connect_errno . ')'
-		.$mysqli->connect_error
+	die('Connect Error ('. $mysqli->connect_errno . ')'
+	    .$mysqli->connect_error
 }
 else {
 	echo "Connection made";
