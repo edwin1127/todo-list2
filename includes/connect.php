dd328818b7318616022f47718424486bdb84e387
Edwin Corzo <edwin11ramirez@gmail.com>
2015-04-24 17:25:22 -0700

    added login \

--- includes/connect.php
+++ includes/connect.php
@@ -1,13 +1,14 @@
 <?php
-//why is this important? why does localhost have to be first?
-$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
-
-if ($mysqli->connect_error) {
-	die('Connect Error ('. $mysqli->connect_errno . ')'
-	    .$mysqli->connect_error);
+//why is this important? Why does localhost have to be first?
+//this is localhost, user name, password, and the database. They have to be in order for the database to even exits
+$mysqli = new mysqli("localhost","root", "root", "todo");
+//$mysqli->connect_error then we want it to die and have this message
+if($mysqli->connect_error){
+die('Connect Error(' . $mysqli->connect_errno .')'
+	.$mysqli->connect_error);
 }
-else {
-	//echo "Connection made";
+else{
+//	echo "Connection Made";
 }
 $mysqli->close();
 
